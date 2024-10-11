48ee544a905b6a8cecda06c3e65e04bf7d6adac5
KauanFortunato <kauanmatiasfortunato@gmail.com>
2024-10-11 20:39:22 +0100

    API Randomizar Pokémons

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+	const [pokemon, setPokemon] = useState(null);
+
+	// Sortear Pokémon da base de dados
+	const chosenPoke = () => {
+		fetch('http://192.168.1.91/PokeGuess/php/api/random-poke.php') // Lembrar de colocar o ip do pc (ipv4)
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.status === 'sorteado') {
+					console.log('Pokémon sorteado com sucesso');
+					setPokemon(data.pokemon);
+				} else {
+					Alert.alert('Erro', 'Não foi possível sortear o Pokémon.');
+				}
+			})
+			.catch((error) => {
+				console.error('Erro na requisição:', error);
+				Alert.alert('Erro', 'Falha ao conectar com o servidor.');
+			});
+	};
+
+	useEffect(() => {
+		chosenPoke();
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
+			{pokemon && (
+				<View style={styles.container}>
+					<Text>Nome: {pokemon.nome}</Text>
+					<Text>Tipo1: {pokemon.tipo1}</Text>
+					<Text>Tipo2: {pokemon.tipo2}</Text>
+					<Text>habitat: {pokemon.habitat}</Text>
+					<Text>cor: {pokemon.cor}</Text>
+					<Text>Evolucao: {pokemon.numero_evolucao}</Text>
+					<Text>Altura: {pokemon.altura}</Text>
+					<Text>Peso: {pokemon.peso}</Text>
+
+					{pokemon.imagem_base64 && (
+						<Image
+							source={{ uri: `data:image/jpeg;base64,${pokemon.imagem_base64}` }}
+							style={{ width: 180, height: 180 }}
+						/>
+					)}
+					<StatusBar style="auto" />
+				</View>
+			)}
 		</View>
 	);
 }
@@ -13,7 +56,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: '#3A3260',
 		alignItems: 'center',
 		justifyContent: 'center',
 	},
---
+++ src/components/chosen-poke/index.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+
+export default function TestChosenPoke() {}
